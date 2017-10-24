ef7f2dc0cac673e5a52c9d16fdb71ab3ba60f34f
jonathanraes <jonathanraes@gmail.com>
2017-10-24 17:31:54 +0200

    simple ros node
    KinectTouch put in ros node that simply echoes hello world message

---
+++ src/kinecttouch.cpp
@@ -0,0 +1,307 @@
+/*
+ * KinectTouch ROS Publisher node
+ */
+// %Tag(FULLTEXT)%
+// %Tag(ROS_HEADER)%
+#include "ros/ros.h"
+// %EndTag(ROS_HEADER)%
+// %Tag(MSG_HEADER)%
+#include "std_msgs/String.h"
+// %EndTag(MSG_HEADER)%
+
+#include <sstream>
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+// openCV
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+using namespace cv;
+
+// openNI
+#include <XnOpenNI.h>
+#include <XnCppWrapper.h>
+using namespace xn;
+#define CHECK_RC(rc, what)							\
+	if (rc != XN_STATUS_OK)							\
+	{									\
+		printf("%s failed: %s\n", what, xnGetStatusString(rc));		\
+		return rc;							\
+	}
+
+
+// TUIO
+
+#include "TuioServer.h"
+using namespace TUIO;
+
+// TODO smoothing using kalman filter
+
+//---------------------------------------------------------------------------
+// Globals
+//---------------------------------------------------------------------------
+
+// OpenNI
+xn::Context xnContext;
+xn::DepthGenerator xnDepthGenerator;
+xn::ImageGenerator xnImgeGenertor;
+
+bool mousePressed = false;
+
+int initOpenNI(const XnChar* fname) {
+	XnStatus nRetVal = XN_STATUS_OK;
+	ScriptNode scriptNode;
+
+	// initialize context
+	nRetVal = xnContext.InitFromXmlFile(fname, scriptNode);
+	CHECK_RC(nRetVal, "InitFromXmlFile");
+
+	// initialize depth generator
+	nRetVal = xnContext.FindExistingNode(XN_NODE_TYPE_DEPTH, xnDepthGenerator);
+	CHECK_RC(nRetVal, "FindExistingNode(XN_NODE_TYPE_DEPTH)");
+
+	// initialize image generator
+	nRetVal = xnContext.FindExistingNode(XN_NODE_TYPE_IMAGE, xnImgeGenertor);
+	CHECK_RC(nRetVal, "FindExistingNode(XN_NODE_TYPE_IMAGE)");
+
+	return 0;
+}
+
+void average(vector<Mat1s>& frames, Mat1s& mean) {
+	Mat1d acc(mean.size());
+	Mat1d frame(mean.size());
+
+	for (unsigned int i=0; i<frames.size(); i++) {
+		frames[i].convertTo(frame, CV_64FC1);
+		acc = acc + frame;
+	}
+
+	acc = acc / frames.size();
+
+	acc.convertTo(mean, CV_16SC1);
+}
+
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+	const unsigned int nBackgroundTrain = 30;
+	const unsigned short touchDepthMin = 10;
+	const unsigned short touchDepthMax = 20;
+	const unsigned int touchMinArea = 50;
+
+	const bool localClientMode = true; 					// connect to a local client
+
+	const double debugFrameMaxDepth = 4000; // maximal distance (in millimeters) for 8 bit debug depth frame quantization
+	const char* windowName = "Debug";
+	const Scalar debugColor0(0,0,128);
+	const Scalar debugColor1(255,0,0);
+	const Scalar debugColor2(255,255,255);
+
+	int xMin = 110;
+	int xMax = 560;
+	int yMin = 120;
+	int yMax = 320;
+
+	Mat1s depth(480, 640); // 16 bit depth (in millimeters)
+	Mat1b depth8(480, 640); // 8 bit depth
+	Mat3b rgb(480, 640); // 8 bit depth
+
+	Mat3b debug(480, 640); // debug visualization
+
+	Mat1s foreground(640, 480);
+	Mat1b foreground8(640, 480);
+
+	Mat1b touch(640, 480); // touch mask
+
+	Mat1s background(480, 640);
+	vector<Mat1s> buffer(nBackgroundTrain);
+
+// %Tag(INIT)%
+  ros::init(argc, argv, "kinecttouch");
+// %EndTag(INIT)%
+
+	initOpenNI("/home/jonathan/catkin_ws/src/kinect_touch/src/niConfig.xml");
+
+// TUIO server object
+
+	TuioServer* tuio;
+	if (localClientMode) {
+		tuio = new TUIO::TuioServer();
+	} else {
+		tuio = new TuioServer("192.168.0.2",3333,false);
+	}
+	TuioTime time;
+
+
+	// create some sliders
+	namedWindow(windowName);
+	createTrackbar("xMin", windowName, &xMin, 640);
+	createTrackbar("xMax", windowName, &xMax, 640);
+	createTrackbar("yMin", windowName, &yMin, 480);
+	createTrackbar("yMax", windowName, &yMax, 480);
+
+	// create background model (average depth)
+	for (unsigned int i=0; i<nBackgroundTrain; i++) {
+		xnContext.WaitAndUpdateAll();
+		depth.data = (uchar*) xnDepthGenerator.GetDepthMap();
+		buffer[i] = depth;
+	}
+	average(buffer, background);
+
+// %Tag(NODEHANDLE)%
+  ros::NodeHandle n;
+// %EndTag(NODEHANDLE)%
+
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+// %Tag(PUBLISHER)%
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+// %EndTag(PUBLISHER)%
+
+// %Tag(LOOP_RATE)%
+  ros::Rate loop_rate(10);
+// %EndTag(LOOP_RATE)%
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+// %Tag(ROS_OK)%
+  int count = 0;
+  while (ros::ok())
+  {
+// %EndTag(ROS_OK)%
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+// %Tag(FILL_MESSAGE)%
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+// %EndTag(FILL_MESSAGE)%
+
+// %Tag(ROSCONSOLE)%
+    ROS_INFO("%s", msg.data.c_str());
+// %EndTag(ROSCONSOLE)%
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+// %Tag(PUBLISH)%
+    chatter_pub.publish(msg);
+// %EndTag(PUBLISH)%
+
+// %Tag(SPINONCE)%
+    ros::spinOnce();
+// %EndTag(SPINONCE)%
+
+
+	// read available data
+		xnContext.WaitAndUpdateAll();
+
+		// update 16 bit depth matrix
+		depth.data = (uchar*) xnDepthGenerator.GetDepthMap();
+		//xnImgeGenertor.GetGrayscale8ImageMap()
+
+
+
+		// update rgb image
+		//rgb.data = (uchar*) xnImgeGenertor.GetRGB24ImageMap(); // segmentation fault here
+		//cvtColor(rgb, rgb, CV_RGB2BGR);
+
+		// extract foreground by simple subtraction of very basic background model
+		foreground = background - depth;
+
+		// find touch mask by thresholding (points that are close to background = touch points)
+		touch = (foreground > touchDepthMin) & (foreground < touchDepthMax);
+
+		// extract ROI
+		Rect roi(xMin, yMin, xMax - xMin, yMax - yMin);
+		Mat touchRoi = touch(roi);
+
+		// find touch points
+		vector< vector<Point2i> > contours;
+		vector<Point2f> touchPoints;
+		findContours(touchRoi, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point2i(xMin, yMin));
+		for (unsigned int i=0; i<contours.size(); i++) {
+			Mat contourMat(contours[i]);
+			// find touch points by area thresholding
+			if ( contourArea(contourMat) > touchMinArea ) {
+				Scalar center = mean(contourMat);
+				Point2i touchPoint(center[0], center[1]);
+				touchPoints.push_back(touchPoint);
+			}
+		}
+
+		// send TUIO cursors
+
+		time = TuioTime::getSessionTime();
+		tuio->initFrame(time);
+
+		for (unsigned int i=0; i<touchPoints.size(); i++) { // touch points
+			float cursorX = (touchPoints[i].x - xMin) / (xMax - xMin);
+			float cursorY = 1 - (touchPoints[i].y - yMin)/(yMax - yMin);
+			printf("Touch found at %f, %f\n", cursorX, cursorY);
+			TuioCursor* cursor = tuio->getClosestTuioCursor(cursorX,cursorY);
+			// TODO improve tracking (don't move cursors away, that might be closer to another touch point)
+			if (cursor == NULL || cursor->getTuioTime() == time) {
+				tuio->addTuioCursor(cursorX,cursorY);
+				printf("Adding cursor\n");
+			} else {
+				tuio->updateTuioCursor(cursor, cursorX, cursorY);
+			}
+		}
+
+		tuio->stopUntouchedMovingCursors();
+		tuio->removeUntouchedStoppedCursors();
+		tuio->commitFrame();
+
+
+		// draw debug frame
+		depth.convertTo(depth8, CV_8U, 255 / debugFrameMaxDepth); // render depth to debug frame
+		cvtColor(depth8, debug, CV_GRAY2BGR);
+		debug.setTo(debugColor0, touch);  // touch mask
+		rectangle(debug, roi, debugColor1, 2); // surface boundaries
+		for (unsigned int i=0; i<touchPoints.size(); i++) { // touch points
+			circle(debug, touchPoints[i], 5, debugColor2, CV_FILLED);
+		}
+
+		// render debug frame (with sliders)
+		imshow(windowName, debug);
+		// imshow("image", rgb);
+// %Tag(RATE_SLEEP)%
+    loop_rate.sleep();
+// %EndTag(RATE_SLEEP)%
+    ++count;
+  }
+
+
+  return 0;
+}
+// %EndTag(FULLTEXT)%
